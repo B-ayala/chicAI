462e378fbcf3c8741f2e37930c5232550aab92b0
Brian Alan Ayala <bayala@biwares.com>
2025-09-05 13:51:23 -0300

    fix: login

--- src/components/Header/NavBar/MenuData/MenuData.tsx
+++ src/components/Header/NavBar/MenuData/MenuData.tsx
@@ -4,6 +4,7 @@ import { Children } from "react";
 const menuData = [
   { label: "Home", url: "/", Children: [] },
   { label: "About", url: "/about", Children: [] },
+  { label: "Products", url: "/products", Children: [] },
   { label: "Login", url: "#", Children: [] },
   { label: "Contact", url: "/contact", Children: [] },
 ];
--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
 import Home from "../pages/Home/Home";
+import Products from "../pages/Products";
 
 const AppRouter: React.FC = () => {
     return (
@@ -8,6 +9,7 @@ const AppRouter: React.FC = () => {
             <Routes>
                 <Route element={<ProtectedRoute />}>
                     <Route path="/" element={<Home />} />
+                    <Route path="/products" element={<Products />} />
                 </Route>
             </Routes>
         </>
