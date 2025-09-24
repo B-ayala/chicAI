37264a7eb3b131f61144d550887f56719c1cb3c5
briangonzales1996 <brianjose00@gmail.com>
2025-09-24 20:35:49 +0200

    Terminando funcionalidad o fix en NavBar antes de mergear

--- src/components/Header/NavBar/NavBar.tsx
+++ src/components/Header/NavBar/NavBar.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { ContactIcon } from '../../../assets/svg/ContactIcon';
