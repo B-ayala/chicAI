4b3e28ed1ad4283a49d10a0b4094c88ef8e78355
Brian Alan Ayala <bayala@biwares.com>
2025-09-25 15:18:39 -0300

    Se agrega cambios de produccion

--- src/root.componets.tsx
+++ src/root.componets.tsx
@@ -3,13 +3,17 @@ import { HashRouter } from "react-router-dom";
 import AppRouter from "./routers/AppRouter";
 import theme from "./theme";
 
+// Determinar si estamos en producción o desarrollo
+const isProduction = window.location.hostname !== 'localhost' && 
+                    window.location.hostname !== '127.0.0.1';
 
+// Usar basename solo en producción (GitHub Pages)
+const baseName = isProduction ? '/chicAI' : '';
 
 const Root: React.FC = () => {
     return (
-        
         <ThemeProvider theme={theme} >
-            <HashRouter basename="/chicAI">
+            <HashRouter basename={baseName}>
                 <AppRouter />
             </HashRouter>
         </ThemeProvider>
